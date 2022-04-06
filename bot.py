de193613f953b7c995ca2cadce1e14dd8b4092c2
Ashton Sidhu <ashton.sidhu1994@gmail.com>
2022-04-06 00:22:44 -0400

    bot poc

---
+++ bot.py
@@ -0,0 +1,17 @@
+import discord
+import os
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content.startswith('!ping'):
+            await message.channel.send('pong!')
+
+intents = discord.Intents.default()
+client = MyClient(intents=intents)
+client.run(os.environ["SIDHULABS_DISCORD_TOKEN"])
